310e3166df34276762814ad3e26511fd44adf4cd
osktgui <osktgui@gmail.com>
2013-06-21 03:20:32 -0500

    fdfdfdfd

--- BasicControllerAndroid/src/com/example/basiccontrollerandroid/HomeActivity.java
+++ BasicControllerAndroid/src/com/example/basiccontrollerandroid/HomeActivity.java
@@ -3,21 +3,19 @@ package com.example.basiccontrollerandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
-	Button btn1;
+public class HomeActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		 
+		setContentView(R.layout.activity_home);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
 
